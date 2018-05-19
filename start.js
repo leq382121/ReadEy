a004a0ad163853c4afa9d90ccab29633477b0f46
leq382121 <leq382121@gmail.com>
2018-05-19 20:07:00 +0300

    changed to tesseract

--- start.js
+++ start.js
@@ -1,18 +1,6 @@
-var OCRAD = require('ocrad.js');
-var Canvas = require('canvas');
-var Image = Canvas.Image;
-var fs = require('fs');
+var Tesseract = require('tesseract.js');
+var myImage = "./image.jpg";
 
-fs.readFile(__dirname + '/image.jpg', function(err, src) {
-  if (err) {
-    throw err;
-  }
-  var img = new Image();
-  img.src = src;
-  var canvas = new Canvas(img.width, img.height);
-  console.log(img.width);
-
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-  console.log(OCRAD(canvas));
-});
+Tesseract.recognize(myImage)
+.progress(function  (p) { console.log('progress', p)    })
+.then(function (result) { console.log('result', result) })
