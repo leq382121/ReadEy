f1bd86e49b8c351c4359b49f96f967d190885aae
leq382121 <leq382121@gmail.com>
2018-05-20 14:03:19 +0300

    added functionality to scan several images, create outputs with their names, delete unnecessary files to avoid confusion in the code

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # ReadEy
 Repository of just having fun with OCR (simply, extracting text from an image). 
 
-# Download all language packs here manualy because it has some errors for now while downloading
+# Trained Language Assets
+
+Download all language packs here manualy because it has some errors for now while downloading
 https://github.com/tesseract-ocr/tessdata
 
 # instalation
 
-
+```sh
 brew install pkg-config cairo libpng jpeg giflib
+```
 
-
+```sh
 sudo PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig/ npm install canvas
+```
 
-
-npm installl
+```sh
+npm install 
+```
--- start.js
+++ start.js
@@ -1,9 +1,28 @@
 var Tesseract = require('tesseract.js');
 var fs = require('fs');
-var myImage = "./image.jpg";
+var imageFolder = "images/";
+var outputFolder = "output/";
+var arrayOfImages = [];
 
-Tesseract.recognize(myImage)
-.progress(function  (p) { console.log('progress', p)    })
-.then(function (result) { 
-  console.log('result', result.text) 
+//deleting small files
+fs.readdirSync(imageFolder).forEach(file => {
+  if ( fs.statSync(imageFolder + "/" + file).size < 20000 ){
+      console.log("Unnecessary file found, skipping");
+      fs.unlink(imageFolder + "/" + file);
+  } else {
+      console.log (file);
+      console.log (fs.statSync(imageFolder + file).size);
+      arrayOfImages.push(file);
+  }
+});
+
+arrayOfImages.forEach(image => {
+  Tesseract.recognize(imageFolder + image)
+    .progress(function  (p) { console.log('progress', p)})
+    .finally(resultOrError => {
+      var wstream = fs.createWriteStream(outputFolder + image + '.txt');
+      wstream.write(resultOrError.text);
+      wstream.end();
+    });
+    return true;
 });
